ab84f7934e9c90e8e0d6c7eb597a155e136962f2
Haylee Salter <hesalter@unc.edu>
2023-09-19 17:43:12 -0400

    Progress on EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,13 +7,35 @@ len_of_word: int = len(secret_word)
 
 wordle: str = input(f"What is your {len_of_word}-letter guess? ")
 len_of_guess: int = len(wordle)
+idx_of_guess: int = 0
+emoji_string: str = ""
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
 
 while len_of_guess != len_of_word:
     if len_of_word != len_of_guess:
         wordle: str = input(f"That was not {len_of_word} letters! Try again: ")
         len_of_guess: int = len(wordle)
+
+while idx_of_guess < len_of_word:
+    guessed_character: bool = False
+    alt_indices: int = 0
+    if str(wordle[idx_of_guess]) == str(secret_word[idx_of_guess]): 
+        emoji_string = emoji_string + GREEN_BOX
+    else:
+        if str(secret_word[alt_indices]) == str(wordle[idx_of_guess]):
+            guessed_character: bool = True
+            emoji_string = emoji_string + YELLOW_BOX
+        else:
+            alt_indices += 1
+            emoji_string = emoji_string + WHITE_BOX
+    idx_of_guess += 1
+
 while len_of_guess == len_of_word: 
     if wordle == secret_word:
+        print(emoji_string)
         exit(print("Woo! You got it!"))
+    print(emoji_string)
     exit(print("Not quite. Play again soon!"))
-    
