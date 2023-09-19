2345e1964cc287b93ffe33c6ac6ae089fd05a747
Haylee Salter <hesalter@unc.edu>
2023-09-19 18:22:11 -0400

    progress on EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -19,18 +19,19 @@ while len_of_guess != len_of_word:
         wordle: str = input(f"That was not {len_of_word} letters! Try again: ")
         len_of_guess: int = len(wordle)
 
-while idx_of_guess < len_of_word:
-    guessed_character: bool = False
-    alt_indices: int = 0
+alt_indices: int = 0
+guessed_character: bool = False
+
+while (idx_of_guess < len_of_word) and guessed_character == False:
     if str(wordle[idx_of_guess]) == str(secret_word[idx_of_guess]): 
         emoji_string = emoji_string + GREEN_BOX
     else:
         if str(secret_word[alt_indices]) == str(wordle[idx_of_guess]):
-            guessed_character: bool = True
-            emoji_string = emoji_string + YELLOW_BOX
+            guessed_character == True
+            emoji_string = emoji_string + WHITE_BOX
         else:
             alt_indices += 1
-            emoji_string = emoji_string + WHITE_BOX
+            emoji_string = emoji_string + YELLOW_BOX
     idx_of_guess += 1
 
 while len_of_guess == len_of_word: 
